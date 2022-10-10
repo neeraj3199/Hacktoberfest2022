b2e59b8c469991cec903ab3969df61fc8bb2d533
Neeraj <75161450+neeraj3199@users.noreply.github.com>
2022-10-10 16:14:04 +0530

    Update sayak01.cpp

--- Pull Here/LeetCode/PalindromeNumber/sayak01.cpp
+++ Pull Here/LeetCode/PalindromeNumber/sayak01.cpp
@@ -17,3 +17,20 @@ public:
         return store == rx;
     }
 };
+class Solution{  
+ public static bollean isPalindrome(String args[],int x){  
+  int r,sum=0,temp;    
+  
+  
+  temp=x;    
+  while(x>0){    
+   r=x%10;  
+   sum=(sum*10)+r;    
+   x=x/10;    
+  }    
+  if(temp==sum)    
+   return true;    
+  else    
+   return false;    
+}  
+}  
